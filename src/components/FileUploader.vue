5b260241d726266a21195040913c5cc1eb9ed702
ozmeks <omehryakov@gmail.com>
2024-05-20 20:59:00 +0200

    improve validations and enhance delete method

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -9,11 +9,32 @@ export default {
   methods: {
     loadFile(event) {
       const file = event.target.files[0];
+      if (!file) return;
+
       const reader = new FileReader();
       reader.onload = (event) => {
-        this.$emit("file-loaded", JSON.parse(event.target.result));
+        try {
+          const data = JSON.parse(event.target.result);
+
+          if (this.isArrayofObjects(data)) {
+            this.$emit("file-loaded", data);
+          } else {
+            alert("Invalid JSON structure. Expected an array of objects.");
+          }
+        } catch (error) {
+          alert("Failed to load file. Please ensure it is a valid JSON.");
+        }
       };
       reader.readAsText(file);
+    },
+    isArrayofObjects(data) {
+      return (
+        Array.isArray(data) &&
+        data.every(
+          (item) =>
+            typeof item === "object" && item !== null && !Array.isArray(item)
+        )
+      );
     }
   }
 };
